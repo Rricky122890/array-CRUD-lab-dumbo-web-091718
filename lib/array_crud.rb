2c180eb568143010cac85cee00953b5d13a6932c
Ricardo Jean-Pierre <rjeanpierre90@gmail.com>
2018-09-06 15:40:05 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,30 +13,30 @@ end
 
 def add_element_to_start_of_array(array, element)
   arr = ["Do", "Re", "Mi", "Fa"]
-  arr.unshift("Um?")
+  arr.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  arr = ["Do", "Re", "Mi", "Fa"]
+  arr = ["arrays!", "Re", "Mi", "arrays!"]
   arr.pop()
 end
 
 def remove_element_from_start_of_array(array)
-  arr = ["Do", "Re", "Mi", "Fa"]
+  arr = ["wow", "Re", "Mi", "wow"]
   arr.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-  arr = ["Do", "Re", "Mi", "Fa"]
+  arr = ["Do", "am", "Mi", "Fa"]
   arr[1]
 end
 
 def retrieve_first_element_from_array(array)
-  arr = ["Do", "Re", "Mi", "Fa"]
+  arr = ["wow", "Re", "Mi", "Fa"]
   arr[0]
 end
 
 def retrieve_last_element_from_array(array)
-  arr = ["Do", "Re", "Mi", "Fa"]
+  arr = ["Do", "Re", "Mi", "arrays!"]
   arr[-1]
 end
