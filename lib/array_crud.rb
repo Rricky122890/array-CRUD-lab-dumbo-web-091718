9c2b554b8ca6db6e852229b0110feddd0be4e459
Ricardo Jean-Pierre <rjeanpierre90@gmail.com>
2018-09-06 15:32:55 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,29 +7,29 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  arr.push("So")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  arr.unshift("Um?")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  arr.pop()
 end
 
 def remove_element_from_start_of_array(array)
-  
+  arr.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  arr[1]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  arr[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  arr[-1]
 end
